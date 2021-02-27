6222b9b597abe5d16b7e3d8be06e90d7cf212d80
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-02-27 20:05:53 +0200

    Add files via upload

---
+++ sat_python_16/pic1.py
@@ -0,0 +1,19 @@
+import turtle
+import time 
+
+t=turtle.Pen()
+#-------------
+
+turtle.bgcolor('black')
+t.pencolor('lime')
+t.forward(100)
+t.left(90)
+t.pencolor('lime')
+t.forward(100)
+t.pencolor('#00F3FF')
+t.circle(30)
+
+#----------------
+time.sleep(1000)
+
+
