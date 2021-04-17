1c33088db3f0d3a858f072eae38991b5af84faf2
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-04-17 20:40:04 +0300

    Add files via upload

--- sat_python_16/snake.py
+++ sat_python_16/snake.py
@@ -1,5 +1,6 @@
 import turtle
 import time
+import random
 
 #--------------------экран-----------------------------------
 screen = turtle.Screen()
@@ -12,22 +13,51 @@ screen.setup(650, 650)
 snake = []
 snake_segment = turtle.Turtle()
 snake_segment.shape('square')
-snake_segment.color('#FF00F7')
+snake_segment.color('#000000')
+snake_segment.penup()
 # добавляет в список голову
 snake.append(snake_segment)
 #--------------------змея------------------------------------
 
+#--------------------еда-------------------------------------
+food = turtle.Turtle()
+food.shape('turtle')
+food.color('#008000')
+food.penup()
+# рандомно телепортирует еду (x, y)
+food.goto(random.randint(-300, 300), random.randint(-300, 300))
+#--------------------еда-------------------------------------
+
 #--------------------управление------------------------------
 screen.onkeypress(lambda: snake[0].setheading(180), 'Left')
+screen.onkeypress(lambda: snake[0].setheading(0), 'Right')
+screen.onkeypress(lambda: snake[0].setheading(90), 'Up')
+screen.onkeypress(lambda: snake[0].setheading(270), 'Down')
 screen.listen()
 #--------------------управление------------------------------
 
 while True:
-    # заставляет двигаться вперед
-	snake[0].forward(50)
+	if snake[0].distance(food) < 20:
+		# рандомно телепортирует еду (x, y)
+		food.goto(random.randint(-300, 300), random.randint(-300, 300))
+		snake_segment = turtle.Turtle()
+		snake_segment.shape('square')
+		snake_segment.color('#00FF00')
+		snake_segment.penup()
+		# добавляет в список голову
+		snake.append(snake_segment)
+	
+	# передвигает туловище за головой по частям
+	for segment in range(len(snake)-1, 0, -1):
+		x = snake[segment-1].xcor()
+		y = snake[segment-1].ycor()
+		snake[segment].goto(x, y)
+	
+	# заставляет двигаться вперед
+	snake[0].forward(20)
 	
 	# обновление экрана
 	screen.update()
 	
 	# управление fps (40 кадров)
-	time.sleep(0.01)
+	time.sleep(0.1)
