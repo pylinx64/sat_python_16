0c95c76bca0207b4b37c087b973693838a448056
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-05-05 13:19:29 +0300

    Add files via upload

---
+++ sat_python_16/hw1.py
@@ -0,0 +1,5 @@
+numbers_veg = ['помидор', 'огурец', 'да']
+
+print(numbers_veg)
+print(numbers_veg[0])
+
--- sat_python_16/snake1.py
+++ sat_python_16/snake1.py
@@ -39,50 +39,62 @@ screen.onkeypress(lambda: snake[0].setheading(270), 'Down')
 screen.listen()
 #--------------------управление------------------------------
 
+FLAG = False
 while True:
-	if snake[0].distance(food) < 20:
-		# рандомно телепортирует еду (x, y)
-		food.goto(random.randint(-300, 300), random.randint(-300, 300))
-		snake_segment = turtle.Turtle()
-		snake_segment.shape('square')
-		snake_segment.color('#00FF00')
-		snake_segment.penup()
-		# добавляет в список голову
-		snake.append(snake_segment)
-	
-	# передвигает туловище за головой по частям
-	for segment in range(len(snake)-1, 0, -1):
-		x = snake[segment-1].xcor()
-		y = snake[segment-1].ycor()
-		snake[segment].goto(x, y)
-	
-	# заставляет двигаться вперед
-	snake[0].forward(10)
-	
-	# обновление экрана
-	screen.update()
-	
-	# вы находитесь тут ------------------
-	# границы экрана
-	x = snake[0].xcor()
-	y = snake[0].ycor()
-	x = int(x)
-	y = int(y)
-	print(f'x:{x} y:{y}')
-	
+    if snake[0].distance(food) < 20:
+        # рандомно телепортирует еду (x, y)
+        food.goto(random.randint(-300, 300), random.randint(-300, 300))
+        snake_segment = turtle.Turtle()
+        snake_segment.shape('square')
+        snake_segment.color('#00FF00')
+        snake_segment.penup()
+        # добавляет в список голову
+        snake.append(snake_segment)
+    
+    # передвигает туловище за головой по частям
+    for segment in range(len(snake)-1, 0, -1):
+        x = snake[segment-1].xcor()
+        y = snake[segment-1].ycor()
+        snake[segment].goto(x, y)
+    
+    # заставляет двигаться вперед
+    snake[0].forward(20)
+    
+    # обновление экрана
+    screen.update()
+    
+    # вы находитесь тут ------------------
+    # границы экрана
+    x = snake[0].xcor()
+    y = snake[0].ycor()
+    x = int(x)
+    y = int(y)
+    print(f'x:{x} y:{y}')
+    
     # проверка вышла ли голова за границы экрана
-	if x > 300:
+    if x > 300:
         # делает красный экран
         # делает надпись на экране
-		break
-	if x < -300:
-		break
-	if y > 300:
-		break
-	if y < -300:
-		break
-	
-	# управление fps (40 кадров)
-    time.sleep(0.05)
+        break
+    if x < -300:
+        break
+    if y > 300:
+        break
+    if y < -300:
+        break
+    
+    # проверяем не коснулись ли мы головой змейки
+    for body in snake[1:]:
+        # хранятся координаты одного туловища
+        coords = body.position()
+        
+        if snake[0].distance(coords) < 10:
+            FLAG = True
+    
+    if FLAG == True:
+        break
+    
+    # управление fps (40 кадров)
+    time.sleep(0.15)
 
-time.sleep(10)
+time.sleep(3)
